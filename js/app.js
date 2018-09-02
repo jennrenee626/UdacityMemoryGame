a9c3cde834e499fb24d8e3b233465c68f89191de
Jennifer <jgladney626@gmail.com>
2018-09-02 13:21:55 -0500

    update code spacing

--- js/app.js
+++ js/app.js
@@ -3,60 +3,96 @@
  */
 const deck = document.querySelector('.deck');
 
+//shuffle deck on page init
+
 let openCards = [];
+
 let matchedCards = [];
 
-//listens for click event on entire deck  (bug - still adding double clicks)
+//listens for click event on entire deck
 deck.addEventListener('click', function(e) {
-    if (e.target.classList.contains('card') && openCards.length < 2 && !openCards.includes(e.target)) {
-        toggleCards(e);
-        addToggledCards(e);
-        cardMatch(e);
-    }});
-    
+    //start timer
+    //display card symbol and add card to openCard array
+        if (e.target.classList.contains('card') && openCards.length < 2 && !openCards.includes(e.target)) {
+            toggleCards(e);
+            addToggledCards(e);
+        }
+        // check for match
+            if (openCards.length === 2 && e.target.classList.contains('open') && e.target.classList.contains('show')) {
+        //match - lock card in open/show
+                if (openCards[0].innerHTML === openCards[1].innerHTML ) {
+                    cardMatch(e);
+                    //add move and display on score-panel
+                }
+        //all matches found?                    
+                    // // if (matchedCards.length === 16) {
+                    // //     alert("All Match");
+                    // }
+                    //     //popup modal - display final moves, time, stars                      
+                    // }
+                    //     if (playAgain===true) {
+                    //         resizeTo(e);
+                    // }
+        //no match - remove cards from openCard array, toggle cards to default
+                else {
+                    cardNoMatch(e);
+                    //add move and display on score-panel
+                }
+        }});
+
+// reset button click in score-panel AND in modal for play again option
+const reset = document.querySelector('.restart');
+reset.addEventListener('click', function(e) {
+    console.log('restart clicked');
+});
+
+// function reset (e) {
+//     moves = 0;
+//     stars = 3
+//     time = reset
+// }
+
+// function moves (e) {
+//     if (openCards ===2) {
+//         //moves ++;
+//         console.log('two clicks=one move');
+//     }};
+
 
 //checks if card, when clicked toggles open/show classes
 function toggleCards (e) {
     //if  (e.target.classList.contains('card') && openCards.length < 2) {
         e.target.classList.toggle('open');
         e.target.classList.toggle('show');
-    };
-
-function toggleCardsMatch (e) {
-        e.target.classList.toggle('match');
-    }
+};
 
 //adds cards to openCards array
 function addToggledCards (e) {
     //if (e.target.classList.contains('card') && openCards.length < 2) {
-        //openCards.push(e.target);
-        openCards.push(e.target.innerHTML);
-    };
+        openCards.push(e.target);
+        //openCards.push(e.target.innerHTML);
+};
 
-//check if cards match -- not working properly -- only working on second card
+//check if cards match
 function cardMatch (e) {
-    if (openCards.length === 2 && openCards[0] === openCards[1]) {
-        e.target.classList.toggle('match'); //changes card to match class
+        openCards[0].classList.add('match');
+        openCards[1].classList.add('match');
+        matchedCards.push(openCards[0]);
+        matchedCards.push(openCards[1]); //moves matched cards to new matchedCards array 
+            openCards = []; //empties openCards array
         console.log('match'); //prints "match"
-        matchedCards.push(e.target.innerHTML); //moves matched cards to new matchedCards array - bug - adding double clicks
-        openCards = []; //empties openCards array
-    } else if (openCards.length === 2 && openCards[0] != openCards[1]) {   
-        setTimeout(() => { //set timeout - only working on one card
-            toggleCards(e); //toggles only one card
+};
+
+function cardNoMatch (e) {
+     setTimeout(() => {
+        openCards[0].classList.toggle('open');
+        openCards[0].classList.toggle('show');
+        openCards[1].classList.toggle('open');
+        openCards[1].classList.toggle('show');
             openCards = [];
         }, 1000); 
         console.log('not match');
-    }};
-
-    if (matchedCards.length === 16) { //not working
-        alert("All Match");
-    };
-
-
-
-//if cardMatch === true
-    //lock in array
-    
+};
 
 // //counts moves (two clicks = one move)
 // // let moves = 0;
@@ -71,18 +107,17 @@ function cardMatch (e) {
 //     addMove.countMoves = function(b) {
 //     moves ++; // increment it
 //     moves.innerHTML = moves;
-//}
+// }
     
-    //when two cards are in the array - add a move
-   // document.getElementsByClassName('moves').innerText/firstChild/innerHTML = moves; 
+//     when two cards are in the array - add a move
+//    document.getElementsByClassName('moves').innerText/firstChild/innerHTML = moves; 
 
 
+// stars
+// if moves <=10 3 star 
+//     moves >=20 2 star
+//     moves >=30 1 star
 
-
-//stars
-//if moves <=10 3 star 
-    //moves >=20 2 star
-    //moves >=30 1 star
 
 
 
@@ -93,7 +128,7 @@ function cardMatch (e) {
  *   - add each card's HTML to the page
  */
 
-// Shuffle function from http://stackoverflow.com/a/2450976
+//Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
