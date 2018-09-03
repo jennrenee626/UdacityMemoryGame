2a31dfb1bb36be98486992f3e9ae2eb2116d1de6
Jennifer <jgladney626@gmail.com>
2018-09-02 21:34:53 -0500

    change moves to 0, star remove and add working

--- js/app.js
+++ js/app.js
@@ -2,11 +2,15 @@
  * Create a list that holds all of your cards
  */
 const deck = document.querySelector('.deck');
+const moveCount = document.querySelector('.moves');
+
+
 
 //shuffle deck on page init
-
+let moves = 0;
 let openCards = [];
 let matchedCards = [];
+
 
 //listens for click event on entire deck
 deck.addEventListener('click', function(e) {
@@ -17,15 +21,16 @@ deck.addEventListener('click', function(e) {
             addToggledCards(e);
         }
         // check for match
-            if (openCards.length === 2 && e.target.classList.contains('open') && e.target.classList.contains('show')) {
+            if (openCards.length == 2 && e.target.classList.contains('open') && e.target.classList.contains('show')) {
         //match - lock card in open/show
                 if (openCards[0].innerHTML === openCards[1].innerHTML ) {
                     cardMatch(e);
                     addMove(e);
-                    //add move and display on score-panel
+                    moveCountStarRemove(e);
                 }
         //all matches found?                    
                     // // if (matchedCards.length === 16) {
+                            //modalWin(e);
                     // //     alert("All Match");
                     // }
                     //     //popup modal - display final moves, time, stars                      
@@ -37,56 +42,108 @@ deck.addEventListener('click', function(e) {
                 else {
                     cardNoMatch(e);
                     addMove(e);
-                    //add move and display on score-panel
+                    moveCountStarRemove(e);
                 }
         }});
 
 // reset button click in score-panel AND in modal for play again option
 const reset = document.querySelector('.restart');
 reset.addEventListener('click', function(e) {
+    //resetCards(e);
+    resetMoves(e);
+    resetStars(e);
+    //shuffle(array); 
+    //timeReset(e);
     console.log('restart clicked');
 });
 
-// function reset (e) {
-//     moves = 0;
-//     stars = 3
-//     time = reset
+function addMove(e) {
+    moves++;
+    moveCount.innerText = moves;        
+    console.log('two clicks=one move');
+    };
+
+function resetMoves(e) {
+    moves = 0;
+    moveCount.innerText = moves;
+    //if moves == 1 - change html text to say move vs moves 
+    console.log('reset move count');
+}
+
+// const cardIsMatched = document.querySelectorAll('.card'); // not working
+// function resetCards(e) {
+//     //card.classList.remove('match');
+//     cardIsMatched.style.cssText = '.deck';
+//     openCards=[];
+//     matchedCards=[];
 // }
 
-function addMove (e) {
-    if (openCards ===2) {
-        //moves ++;
-        console.log('two clicks=one move');
+let starList = document.querySelector('.stars');   // Get the <ul> element with class ="stars" not working
+
+function moveCountStarRemove (e) {
+    if (moves < 2){
+        console.log("moves less than 15");
+    } else if (moves === 3) {
+        removeStar(e);
+    } else if (moves === 5) {
+        removeStar(e);
+    } else if (moves === 7) {
+        removeStar(e);
     }};
+
+function removeStar(e){
+    starList.removeChild(starList.children[0]);
+};
+
+function resetStars(e) {
+        let li = document.createElement('li');
+        li.innerHTML = '<i class="fa fa-star"></i>';
+        //for (var s = 0; i <3; i++)
+        starList.appendChild(li);
+
+};
+
+// function timer(e) {
+
+// };
+
+// function timeReset(e) {
+
+// };
+
+// function modalWin(e) {
+
+// };
+
 
 
 //checks if card, when clicked toggles open/show classes
-function toggleCards (e) {
+function toggleCards(e) {
     //if  (e.target.classList.contains('card') && openCards.length < 2) {
         e.target.classList.toggle('open');
         e.target.classList.toggle('show');
 };
 
 //adds cards to openCards array
-function addToggledCards (e) {
+function addToggledCards(e) {
     //if (e.target.classList.contains('card') && openCards.length < 2) {
         openCards.push(e.target);
         //openCards.push(e.target.innerHTML);
 };
 
 //check if cards match
-function cardMatch (e) {
+function cardMatch(e) {
         openCards[0].classList.add('match');
         openCards[1].classList.add('match');
         matchedCards.push(openCards[0]);
         matchedCards.push(openCards[1]); //moves matched cards to new matchedCards array 
             openCards = []; //empties openCards array
-        console.log('match'); //prints "match"
+        console.log('match');
 };
 
-function cardNoMatch (e) {
+function cardNoMatch(e) {
      setTimeout(() => {
-        openCards[0].classList.toggle('open');
+        openCards[0].classList.toggle('open'); //toggles first item in array back to default
         openCards[0].classList.toggle('show');
         openCards[1].classList.toggle('open');
         openCards[1].classList.toggle('show');
@@ -94,33 +151,6 @@ function cardNoMatch (e) {
         }, 1000); 
         console.log('not match');
 };
-
-// //counts moves (two clicks = one move)
-// // let moves = 0;
-
-// // function countMoves (e) {
-// //         if (openCards === 2) {
-// //         moves ++;
-// //     }};
-
-//     let moves = 0; // counter 
-//     const addMove = document.querySelector('.moves'); // element
-//     addMove.countMoves = function(b) {
-//     moves ++; // increment it
-//     moves.innerHTML = moves;
-// }
-    
-//     when two cards are in the array - add a move
-//    document.getElementsByClassName('moves').innerText/firstChild/innerHTML = moves; 
-
-
-// stars
-// if moves <=10 3 star 
-//     moves >=20 2 star
-//     moves >=30 1 star
-
-
-
 
 /*
  * Display the cards on the page
